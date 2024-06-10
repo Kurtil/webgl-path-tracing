1eb6df3a69b618b9b9cbfe627daa6eb13f0d723f
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2024-06-10 19:39:20 +0200

    Vector class

--- math/Vector.js
+++ math/Vector.js
@@ -1,139 +1,189 @@
 // TODO remove
 var Sylvester = {
-    version: '0.1.3',
-    precision: 1e-6
+  version: '0.1.3',
+  precision: 1e-6
+};
+
+class Vector {
+  // i, j, k unit vectors
+  static i = Vector.create([1, 0, 0]);
+  static j = Vector.create([0, 1, 0]);
+  static k = Vector.create([0, 0, 1]);
+
+  // Random vector of size n
+  static Random(n) {
+    var elements = [];
+    do {
+      elements.push(Math.random());
+    } while (--n);
+    return Vector.create(elements);
   };
 
-function Vector() {}
-Vector.prototype = {
+  // Vector filled with zeros
+  static Zero(n) {
+    var elements = [];
+    do {
+      elements.push(0);
+    } while (--n);
+    return Vector.create(elements);
+  };
+
+  static min(a, b) {
+    if (a.length != b.length) {
+      return null;
+    }
+    var newElements = [];
+    for (var i = 0; i < a.elements.length; i++) {
+      newElements.push(Math.min(a.elements[i], b.elements[i]));
+    }
+    return Vector.create(newElements);
+  };
+
+  static max(a, b) {
+    if (a.length != b.length) {
+      return null;
+    }
+    var newElements = [];
+    for (var i = 0; i < a.elements.length; i++) {
+      newElements.push(Math.max(a.elements[i], b.elements[i]));
+    }
+    return Vector.create(newElements);
+  };
+
+  static create(elements) {
+    var V = new Vector();
+    return V.setElements(elements);
+  };
+
 
   // Returns element i of the vector
-  e: function(i) {
-    return (i < 1 || i > this.elements.length) ? null : this.elements[i-1];
-  },
+  e(i) {
+    return (i < 1 || i > this.elements.length) ? null : this.elements[i - 1];
+  }
 
   // Returns the number of elements the vector has
-  dimensions: function() {
+  dimensions() {
     return this.elements.length;
-  },
+  }
 
   // Returns the modulus ('length') of the vector
-  modulus: function() {
+  modulus() {
     return Math.sqrt(this.dot(this));
-  },
+  }
 
   // Returns true iff the vector is equal to the argument
-  eql: function(vector) {
+  eql(vector) {
     var n = this.elements.length;
     var V = vector.elements || vector;
     if (n != V.length) { return false; }
     do {
-      if (Math.abs(this.elements[n-1] - V[n-1]) > Sylvester.precision) { return false; }
+      if (Math.abs(this.elements[n - 1] - V[n - 1]) > Sylvester.precision) { return false; }
     } while (--n);
     return true;
-  },
+  }
 
   // Returns a copy of the vector
-  dup: function() {
+  dup() {
     return Vector.create(this.elements);
-  },
+  }
 
   // Maps the vector to another vector according to the given function
-  map: function(fn) {
+  map(fn) {
     var elements = [];
-    this.each(function(x, i) {
+    this.each(function (x, i) {
       elements.push(fn(x, i));
     });
     return Vector.create(elements);
-  },
-  
+  }
+
   // Calls the iterator for each element of the vector in turn
-  each: function(fn) {
+  each(fn) {
     var n = this.elements.length, k = n, i;
-    do { i = k - n;
-      fn(this.elements[i], i+1);
+    do {
+      i = k - n;
+      fn(this.elements[i], i + 1);
     } while (--n);
-  },
+  }
 
   // Returns a new vector created by normalizing the receiver
-  toUnitVector: function() {
+  toUnitVector() {
     var r = this.modulus();
     if (r === 0) { return this.dup(); }
-    return this.map(function(x) { return x/r; });
-  },
+    return this.map(function (x) { return x / r; });
+  }
 
   // Returns the angle between the vector and the argument (also a vector)
-  angleFrom: function(vector) {
+  angleFrom(vector) {
     var V = vector.elements || vector;
     var n = this.elements.length, k = n, i;
     if (n != V.length) { return null; }
     var dot = 0, mod1 = 0, mod2 = 0;
     // Work things out in parallel to save time
-    this.each(function(x, i) {
-      dot += x * V[i-1];
+    this.each(function (x, i) {
+      dot += x * V[i - 1];
       mod1 += x * x;
-      mod2 += V[i-1] * V[i-1];
+      mod2 += V[i - 1] * V[i - 1];
     });
     mod1 = Math.sqrt(mod1); mod2 = Math.sqrt(mod2);
-    if (mod1*mod2 === 0) { return null; }
-    var theta = dot / (mod1*mod2);
+    if (mod1 * mod2 === 0) { return null; }
+    var theta = dot / (mod1 * mod2);
     if (theta < -1) { theta = -1; }
     if (theta > 1) { theta = 1; }
     return Math.acos(theta);
-  },
+  }
 
   // Returns true iff the vector is parallel to the argument
-  isParallelTo: function(vector) {
+  isParallelTo(vector) {
     var angle = this.angleFrom(vector);
     return (angle === null) ? null : (angle <= Sylvester.precision);
-  },
+  }
 
   // Returns true iff the vector is antiparallel to the argument
-  isAntiparallelTo: function(vector) {
+  isAntiparallelTo(vector) {
     var angle = this.angleFrom(vector);
     return (angle === null) ? null : (Math.abs(angle - Math.PI) <= Sylvester.precision);
-  },
+  }
 
   // Returns true iff the vector is perpendicular to the argument
-  isPerpendicularTo: function(vector) {
+  isPerpendicularTo(vector) {
     var dot = this.dot(vector);
     return (dot === null) ? null : (Math.abs(dot) <= Sylvester.precision);
-  },
+  }
 
   // Returns the result of adding the argument to the vector
-  add: function(vector) {
+  add(vector) {
     var V = vector.elements || vector;
     if (this.elements.length != V.length) { return null; }
-    return this.map(function(x, i) { return x + V[i-1]; });
-  },
+    return this.map(function (x, i) { return x + V[i - 1]; });
+  }
 
   // Returns the result of subtracting the argument from the vector
-  subtract: function(vector) {
+  subtract(vector) {
     var V = vector.elements || vector;
     if (this.elements.length != V.length) { return null; }
-    return this.map(function(x, i) { return x - V[i-1]; });
-  },
+    return this.map(function (x, i) { return x - V[i - 1]; });
+  }
 
   // Returns the result of multiplying the elements of the vector by the argument
-  multiply: function(k) {
-    return this.map(function(x) { return x*k; });
-  },
+  multiply(k) {
+    return this.map(function (x) { return x * k; });
+  }
 
-  x: function(k) { return this.multiply(k); },
+  x(k) { return this.multiply(k); }
 
   // Returns the scalar product of the vector with the argument
   // Both vectors must have equal dimensionality
-  dot: function(vector) {
+  dot(vector) {
     var V = vector.elements || vector;
     var i, product = 0, n = this.elements.length;
     if (n != V.length) { return null; }
-    do { product += this.elements[n-1] * V[n-1]; } while (--n);
+    do { product += this.elements[n - 1] * V[n - 1]; } while (--n);
     return product;
-  },
+  }
 
   // Returns the vector product of the vector with the argument
   // Both vectors must have dimensionality 3
-  cross: function(vector) {
+  cross(vector) {
     var B = vector.elements || vector;
     if (this.elements.length != 3 || B.length != 3) { return null; }
     var A = this.elements;
@@ -142,72 +192,74 @@ Vector.prototype = {
       (A[2] * B[0]) - (A[0] * B[2]),
       (A[0] * B[1]) - (A[1] * B[0])
     ]);
-  },
+  }
 
   // Returns the (absolute) largest element of the vector
-  max: function() {
+  max() {
     var m = 0, n = this.elements.length, k = n, i;
-    do { i = k - n;
+    do {
+      i = k - n;
       if (Math.abs(this.elements[i]) > Math.abs(m)) { m = this.elements[i]; }
     } while (--n);
     return m;
-  },
+  }
 
   // Returns the index of the first match found
-  indexOf: function(x) {
+  indexOf(x) {
     var index = null, n = this.elements.length, k = n, i;
-    do { i = k - n;
+    do {
+      i = k - n;
       if (index === null && this.elements[i] == x) {
         index = i + 1;
       }
     } while (--n);
     return index;
-  },
+  }
 
   // Returns a diagonal matrix with the vector's elements as its diagonal elements
-  toDiagonalMatrix: function() {
+  toDiagonalMatrix() {
     return Matrix.Diagonal(this.elements);
-  },
+  }
 
   // Returns the result of rounding the elements of the vector
-  round: function() {
-    return this.map(function(x) { return Math.round(x); });
-  },
+  round() {
+    return this.map(function (x) { return Math.round(x); });
+  }
 
   // Returns a copy of the vector with elements set to the given value if they
   // differ from it by less than Sylvester.precision
-  snapTo: function(x) {
-    return this.map(function(y) {
+  snapTo(x) {
+    return this.map(function (y) {
       return (Math.abs(y - x) <= Sylvester.precision) ? x : y;
     });
-  },
+  }
 
   // Returns the vector's distance from the argument, when considered as a point in space
-  distanceFrom: function(obj) {
+  distanceFrom(obj) {
     if (obj.anchor) { return obj.distanceFrom(this); }
     var V = obj.elements || obj;
     if (V.length != this.elements.length) { return null; }
     var sum = 0, part;
-    this.each(function(x, i) {
-      part = x - V[i-1];
+    this.each(function (x, i) {
+      part = x - V[i - 1];
       sum += part * part;
     });
     return Math.sqrt(sum);
-  },
+  }
 
   // Returns true if the vector is point on the given line
-  liesOn: function(line) {
+  liesOn(line) {
     return line.contains(this);
-  },
+  }
 
   // Return true iff the vector is a point in the given plane
-  liesIn: function(plane) {
+  liesIn(plane) {
     return plane.contains(this);
-  },
+  }
 
   // Rotates the vector about the given object. The object should be a 
   // point if the vector is 2D, and a line if it is 3D. Be careful with line directions!
-  rotate: function(t, obj) {
+  rotate(t, obj) {
     var V, R, x, y, z;
     switch (this.elements.length) {
       case 2:
@@ -237,10 +289,10 @@ Vector.prototype = {
       default:
         return null;
     }
-  },
+  }
 
   // Returns the result of reflecting the point in the given point, line or plane
-  reflectionIn: function(obj) {
+  reflectionIn(obj) {
     if (obj.anchor) {
       // obj is a plane or line
       var P = this.elements.slice();
@@ -250,12 +302,12 @@ Vector.prototype = {
       // obj is a point
       var Q = obj.elements || obj;
       if (this.elements.length != Q.length) { return null; }
-      return this.map(function(x, i) { return Q[i-1] + (Q[i-1] - x); });
+      return this.map(function (x, i) { return Q[i - 1] + (Q[i - 1] - x); });
     }
-  },
+  }
 
   // Utility to make sure vectors are 3D. If they are 2D, a zero z-component is added
-  to3D: function() {
+  to3D() {
     var V = this.dup();
     switch (V.elements.length) {
       case 3: break;
@@ -263,115 +315,65 @@ Vector.prototype = {
       default: return null;
     }
     return V;
-  },
+  }
 
   // Returns a string representation of the vector
-  inspect: function() {
+  inspect() {
     return '[' + this.elements.join(', ') + ']';
-  },
+  }
 
   // Set vector's elements from an array
-  setElements: function(els) {
+  setElements(els) {
     this.elements = (els.elements || els).slice();
     return this;
   }
-};
-  
-// Constructor function
-Vector.create = function(elements) {
-  var V = new Vector();
-  return V.setElements(elements);
-};
 
-// i, j, k unit vectors
-Vector.i = Vector.create([1,0,0]);
-Vector.j = Vector.create([0,1,0]);
-Vector.k = Vector.create([0,0,1]);
+  minComponent() {
+    var value = Number.MAX_VALUE;
+    for (var i = 0; i < this.elements.length; i++) {
+      value = Math.min(value, this.elements[i]);
+    }
+    return value;
+  };
 
-// Random vector of size n
-Vector.Random = function(n) {
-  var elements = [];
-  do { elements.push(Math.random());
-  } while (--n);
-  return Vector.create(elements);
-};
+  maxComponent() {
+    var value = -Number.MAX_VALUE;
+    for (var i = 0; i < this.elements.length; i++) {
+      value = Math.max(value, this.elements[i]);
+    }
+    return value;
+  };
 
-// Vector filled with zeros
-Vector.Zero = function(n) {
-  var elements = [];
-  do { elements.push(0);
-  } while (--n);
-  return Vector.create(elements);
-};
 
-Vector.prototype.ensure3 = function () {
+  flatten() {
+    return this.elements;
+  };
+
+  ensure3() {
     return Vector.create([this.elements[0], this.elements[1], this.elements[2]]);
-};
+  };
 
-Vector.prototype.ensure4 = function (w) {
+  ensure4(w) {
     return Vector.create([this.elements[0], this.elements[1], this.elements[2], w]);
-};
+  };
 
-Vector.prototype.divideByW = function () {
+  divideByW() {
     var w = this.elements[this.elements.length - 1];
     var newElements = [];
     for (var i = 0; i < this.elements.length; i++) {
-        newElements.push(this.elements[i] / w);
+      newElements.push(this.elements[i] / w);
     }
     return Vector.create(newElements);
-};
+  };
 
-Vector.prototype.componentDivide = function (vector) {
+  componentDivide(vector) {
     if (this.elements.length != vector.elements.length) {
-        return null;
+      return null;
     }
     var newElements = [];
     for (var i = 0; i < this.elements.length; i++) {
-        newElements.push(this.elements[i] / vector.elements[i]);
+      newElements.push(this.elements[i] / vector.elements[i]);
     }
     return Vector.create(newElements);
-};
-
-Vector.min = function (a, b) {
-    if (a.length != b.length) {
-        return null;
-    }
-    var newElements = [];
-    for (var i = 0; i < a.elements.length; i++) {
-        newElements.push(Math.min(a.elements[i], b.elements[i]));
-    }
-    return Vector.create(newElements);
-};
-
-Vector.max = function (a, b) {
-    if (a.length != b.length) {
-        return null;
-    }
-    var newElements = [];
-    for (var i = 0; i < a.elements.length; i++) {
-        newElements.push(Math.max(a.elements[i], b.elements[i]));
-    }
-    return Vector.create(newElements);
-};
-
-Vector.prototype.minComponent = function () {
-    var value = Number.MAX_VALUE;
-    for (var i = 0; i < this.elements.length; i++) {
-        value = Math.min(value, this.elements[i]);
-    }
-    return value;
-};
-
-Vector.prototype.maxComponent = function () {
-    var value = -Number.MAX_VALUE;
-    for (var i = 0; i < this.elements.length; i++) {
-        value = Math.max(value, this.elements[i]);
-    }
-    return value;
-};
-
-
-Vector.prototype.flatten = function ()
-{
-    return this.elements;
+  };
 };
