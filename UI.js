9ad687c178e5d8164892826b95ae6cae1da7af46
Gaëtan Lagier <gaetan.lagier@hotmail.fr>
2024-06-10 23:08:42 +0200

    move some main code into UI

--- UI.js
+++ UI.js
@@ -2,6 +2,68 @@ class UI {
     constructor() {
         this.renderer = new Renderer();
         this.moving = false;
+
+        var mouseDown = false, oldX, oldY;
+
+        document.onmousedown = function (event) {
+            var mouse = canvasMousePos(event);
+            oldX = mouse.x;
+            oldY = mouse.y;
+
+            if (mouse.x >= 0 && mouse.x < 512 && mouse.y >= 0 && mouse.y < 512) {
+                mouseDown = !ui.mouseDown(mouse.x, mouse.y);
+
+                // disable selection because dragging is used for rotating the camera and moving objects
+                return false;
+            }
+
+            return true;
+        };
+
+        document.onmousemove = function (event) {
+            var mouse = canvasMousePos(event);
+
+            if (mouseDown) {
+                // update the angles based on how far we moved since last time
+                angleY -= (mouse.x - oldX) * 0.01;
+                angleX += (mouse.y - oldY) * 0.01;
+
+                // don't go upside down
+                angleX = Math.max(angleX, -Math.PI / 2 + 0.01);
+                angleX = Math.min(angleX, Math.PI / 2 - 0.01);
+
+                // clear the sample buffer
+                ui.renderer.pathTracer.sampleCount = 0;
+
+                // remember this coordinate
+                oldX = mouse.x;
+                oldY = mouse.y;
+            } else {
+                var canvasPos = elementPos(canvas);
+                ui.mouseMove(mouse.x, mouse.y);
+            }
+        };
+
+        document.onmouseup = function (event) {
+            mouseDown = false;
+
+            var mouse = canvasMousePos(event);
+            ui.mouseUp(mouse.x, mouse.y);
+        };
+
+        document.onkeydown = function (event) {
+            // if there are no <input> elements focused
+            if (inputFocusCount == 0) {
+                // if backspace or delete was pressed
+                if (event.keyCode == 8 || event.keyCode == 46) {
+                    ui.deleteSelection();
+
+                    // don't let the backspace key go back a page
+                    return false;
+                }
+            }
+        };
+
     }
 
     setObjects(objects) {
@@ -140,5 +202,31 @@ class UI {
             this.renderer.pathTracer.sampleCount = 0;
         }
         glossiness = newGlossiness;
+    };
+}
+
+function elementPos(element) {
+    var x = 0, y = 0;
+    while (element.offsetParent) {
+        x += element.offsetLeft;
+        y += element.offsetTop;
+        element = element.offsetParent;
+    }
+    return { x: x, y: y };
+}
+
+function eventPos(event) {
+    return {
+        x: event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
+        y: event.clientY + document.body.scrollTop + document.documentElement.scrollTop
+    };
+}
+
+function canvasMousePos(event) {
+    var mousePos = eventPos(event);
+    var canvasPos = elementPos(canvas);
+    return {
+        x: mousePos.x - canvasPos.x,
+        y: mousePos.y - canvasPos.y
     };
 }
